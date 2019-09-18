024967dc1a44fc76a8f55d9c047c5fd520a06ac6
jennienguyen <jennien@bu.edu>
2019-09-17 22:08:56 -0400

    add finished solutions for ps1

--- PS1/P1.js
+++ PS1/P1.js
@@ -1,18 +1,2 @@
-//using function
-function abcOrdering(string) {
-    return string.replace(/[&\/\\#,+()$~%.'":*?<>{}!]/g,'').split('').sort().join('');
-}
-console.log(`${abcOrdering("supercalifragilisticexpialidocious")}`);
-
-//using const
-const abcOrdering2 = function (string) {
-    return string.replace(/[&\/\\#,+()$~%.'":*?<>{}!]/g,'').split('').sort().join('');
-}
-console.log(`${abcOrdering2("supercalifragilisticexpialidocious")}`);
-
-//using =>
 const abcOrdering3 = string => string.replace(/[&\/\\#,+()$~%.'":*?<>{}!]/g,'').split('').sort().join('');
-console.log(`${abcOrdering3("supercalifragilisticexpialidocious")}`);
-
-
-
+console.log(`${abcOrdering3("supercalifragilis!ticexpialidocious")}`);
--- PS1/P2.js
+++ PS1/P2.js
@@ -1,9 +1,41 @@
 //write a function that takes a string of a math expression and return the value of the expression
 
-function addbits(s) {
-    var total = 0;
-    s = s.replace(/\s/g, '').match(/[+\-]?([0-9\.\s]+)/g) || [];
-    while(s.length) total += parseFloat(s.shift());
-    return total;
+const evaluate = string => {
+    let operator = string.charAt(1);
+    let left = (parseInt(string.charAt(0)));
+    let right = parseInt(string.charAt(2));
+    switch (operator) {
+        case '+':
+            return (left, right) => left + right;
+            break;
+        case '*':
+            return (left, right) => left * right;
+            break;
+        case '-':
+            return (left,right) => left - right;
+            break;
+        case '/':
+            return (left,right) => left / right;
+            break;
+        case '%':
+            return (left,right) => left % right;
+            break;
+    }
 }
-console.log(`${addbits('8%3')}`);
+
+const expression1 = '4+2';
+let operator1 = evaluate(expression1);
+console.log(`${expression1} = ${operator1((parseInt(expression1.charAt(0))),(parseInt(expression1.charAt(2))))}`);
+
+const expression2 = '5*7';
+let operator2 = evaluate(expression2);
+console.log(`${expression2} = ${operator2((parseInt(expression2.charAt(0))),(parseInt(expression2.charAt(2))))}`);
+
+const expression3 = '6-1';
+let operator3 = evaluate(expression3);
+console.log(`${expression3} = ${operator3((parseInt(expression3.charAt(0))),(parseInt(expression3.charAt(2))))}`);
+
+const expression4 = '9-2';
+let operator4 = evaluate(expression4);
+console.log(`${expression4} = ${operator4((parseInt(expression4.charAt(0))),(parseInt(expression4.charAt(2))))}`);
+
